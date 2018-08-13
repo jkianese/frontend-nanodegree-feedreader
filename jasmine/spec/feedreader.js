46edc1037791b5eb6681caff42d68612bc5814a6
Jim Kianese <jkianese@gmail.com>
2018-08-12 21:16:51 -0400

    First Three Jasmine Tests Firing
    First Three Jasmine Tests Firing

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -26,27 +26,42 @@ $(function() {
             expect(allFeeds.length).not.toBe(0);
         });
 
-
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a URL defined
          * and that the URL is not empty.
          */
+        it('URL is defined and URL is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.url).toBeDefined();
+                expect(feed.url.length).not.toBe(0);  
+            }); 
+        }); 
 
 
         /* TODO: Write a test that loops through each feed
          * in the allFeeds object and ensures it has a name defined
          * and that the name is not empty.
-         */
+         
+        it('name is defined and name is not empty', function() {
+            allFeeds.forEach(function(feed) {
+                expect(feed.name).toBeDefined();
+                expect(feed.name).not.toBe(''); 
+            });
+        });
+        */
     });
 
 
     /* TODO: Write a new test suite named "The menu" */
-
+    describe('the menu', function() {
         /* TODO: Write a test that ensures the menu element is
          * hidden by default. You'll have to analyze the HTML and
          * the CSS to determine how we're performing the
          * hiding/showing of the menu element.
          */
+        // it('the menu element is hidden by default', function() {
+
+        // });
 
          /* TODO: Write a test that ensures the menu changes
           * visibility when the menu icon is clicked. This test
@@ -69,4 +84,5 @@ $(function() {
          * by the loadFeed function that the content actually changes.
          * Remember, loadFeed() is asynchronous.
          */
+    });     
 }());
