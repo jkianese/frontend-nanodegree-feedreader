5ae4c42c67a0e50a131862d032277e2c8eedd2fd
Jim Kianese <jkianese@gmail.com>
2018-08-17 20:10:06 -0400

    asynchronous implementation
    asynchronous implementation

--- jasmine/spec/feedreader.js
+++ jasmine/spec/feedreader.js
@@ -25,11 +25,7 @@ $(function() {
             expect(allFeeds).toBeDefined();
             expect(allFeeds.length).not.toBe(0);
         });
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a URL defined
-         * and that the URL is not empty.
-         */
+        
         it('URL is defined and URL is not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.url).toBeDefined();
@@ -37,11 +33,6 @@ $(function() {
             }); 
         }); 
 
-
-        /* TODO: Write a test that loops through each feed
-         * in the allFeeds object and ensures it has a name defined
-         * and that the name is not empty.
-         */
         it('name is defined and name is not empty', function() {
             allFeeds.forEach(function(feed) {
                 expect(feed.name).toBeDefined();
@@ -50,24 +41,14 @@ $(function() {
         });
     });
 
-    /* TODO: Write a new test suite named "The menu" */
     describe('the menu', function() {
         const body = document.body;
         const menu = document.querySelector(".menu-icon-link");  
-        /* TODO: Write a test that ensures the menu element is
-         * hidden by default. You'll have to analyze the HTML and
-         * the CSS to determine how we're performing the
-         * hiding/showing of the menu element.
-         */
+       
         it('the menu element is hidden by default', function() {
             expect(body.classList.contains('menu-hidden')).toBe(true); 
         });
 
-         /* TODO: Write a test that ensures the menu changes
-          * visibility when the menu icon is clicked. This test
-          * should have two expectations: does the menu display when
-          * clicked and does it hide when clicked again.
-          */
          it('menu toggles on and off', function() {
             menu.click(); 
             expect(body.classList.contains('menu-hidden')).toBe(false); 
@@ -75,49 +56,38 @@ $(function() {
             expect(body.classList.contains('menu-hidden')).toBe(true); 
          }); 
     });
-    
-    /* TODO: Write a new test suite named "Initial Entries" */
+   
     describe('Initial Entries', function() {
-        const feed = document.querySelector('.feed');  
-        /* TODO: Write a test that ensures when the loadFeed
-         * function is called and completes its work, there is at least
-         * a single .entry element within the .feed container.
-         * Remember, loadFeed() is asynchronous so this test will require
-         * the use of Jasmine's beforeEach and asynchronous done() function.
-         */
+
         beforeEach(function(done) {
             loadFeed(0, done);
         });
 
         it('loadFeed is called and completes its work', function() {
-            expect(feed.children.length > 0).toBe(true); 
+            const feed = document.querySelectorAll('.feed .entry'); 
+            expect(feed.length).not.toBe(0); 
         });
     });             
-    /* TODO: Write a new test suite named "New Feed Selection" */
-    describe('New Feed Selection', function() {        
-        /* TODO: Write a test that ensures when a new feed is loaded
-         * by the loadFeed function that the content actually changes.
-         * Remember, loadFeed() is asynchronous.
-         */
 
-        const initialFeed = [];
+    describe('New Feed Selection', function() {        
+        
+        let initialFeed = []; 
+        let newFeed = []; 
 
         beforeEach(function(done) {
-            
             loadFeed(0, function() {
                 initialFeed = document.querySelector('.feed').innerHTML;
-            });   
-            
-            loadFeed(1, function() { 
-                done(); 
-            });
-        }); 
 
-        it('content changes when new feed is loaded', function() {
-            const newFeed = document.querySelector('.feed').innerHTML 
-            expect(newFeed).not.toBe(initialFeed); 
+                loadFeed(1, function() {
+                    newFeed = document.querySelector('.feed').innerHTML; 
+                    done(); 
+                }); 
+            });
         });
-    });     
+        it('content changes when new feed is loaded', function() {
+            expect(newFeed).toBe(initialFeed); 
+        });
+    });    
 }());
 
 /*  Resources consulted during completion of this project:
@@ -129,4 +99,5 @@ $(function() {
  *  https://www.youtube.com/watch?v=_XwH-xfvydw
  *
  *  Grow with Google and Udacity FEND Resources<br>
- *  https://www.diigo.com/outliner/fjsk23/Udacity-Feed-Reader-Testing-(project-%234)?key=i5xqspbzvg    
+ *  https://www.diigo.com/outliner/fjsk23/Udacity-Feed-Reader-Testing-(project-%234)?key=i5xqspbzvg
+ */    
